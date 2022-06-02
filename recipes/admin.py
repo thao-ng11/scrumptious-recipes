f146cc23204685ac6a1f2e4658d299db9d0d2ade
Thao <thao.ng11@gmail.com>
2022-06-02 13:03:18 -0400

    Scrumptious Recipes

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,4 +1,25 @@
+from logging import StringTemplateStyle
 from django.contrib import admin
-
+from recipes.models import Recipe, Step, Ingredient
 
 # Register your models here.
+
+# Create a class that inherit from admin.ModelAdmin
+
+
+class RecipeAdmin(admin.ModelAdmin):
+    pass
+
+
+class StepAdmin(admin.ModelAdmin):
+    pass
+
+
+class IngredientAdmin(admin.ModelAdmin):
+    pass
+
+
+# Register the class and  model
+admin.site.register(Recipe, RecipeAdmin)
+admin.site.register(Step, StepAdmin)
+admin.site.register(Ingredient, IngredientAdmin)
--- recipes/models.py
+++ recipes/models.py
@@ -1,4 +1,75 @@
 from django.db import models
 
+# One
+class Recipe(models.Model):
+    name = models.CharField(max_length=125)
+    author = models.CharField(max_length=100)
+    description = models.TextField()
+    image = models.URLField(
+        null=True, blank=True
+    )  # blank allowed to have image as optional
+    created = models.DateTimeField(auto_now_add=True)  # record the timestamp
+    updated = models.DateTimeField(auto_now=True)
 
-# Create your models here.
+    def __str__(self):
+        return self.name + " by " + self.author
+
+
+# Many
+class Step(models.Model):
+    recipe = models.ForeignKey(
+        "Recipe",
+        blank=True,
+        null=True,
+        related_name="steps",
+        on_delete=models.CASCADE,
+    )
+    order = models.PositiveSmallIntegerField()
+    directions = models.TextField(max_length=300)
+
+    def __str__(self):
+        return self.recipe + self.order + self.directions
+
+
+class Measure(models.Model):
+
+    name = models.CharField(max_length=30, unique=True)
+    abbreviation = models.CharField(max_length=10, unique=True)
+
+    def __str__(self):
+        return self.name + " " + self.abbreviation
+
+
+class FoodItem(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Ingredient(models.Model):
+    amount = models.FloatField
+    recipe = models.ForeignKey(
+        "Recipe",
+        related_name="ingredients",
+        on_delete=models.CASCADE,
+    )
+    measure = models.ForeignKey(
+        "Measure",
+        on_delete=models.PROTECT,
+    )
+    food = models.ForeignKey(
+        "FoodItem",
+        on_delete=models.PROTECT,
+    )
+
+    def __str__(self):
+        return (
+            self.amount
+            + " "
+            + self.recipe
+            + " "
+            + self.measure
+            + " "
+            + self.food
+        )
