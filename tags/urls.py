d3f0ff596e94f7f23db33f54d5f8d1ccf6ff5fd8
Thao <thao.ng11@gmail.com>
2022-06-03 18:24:08 -0400

    Show tag view

--- tags/urls.py
+++ tags/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from tags.views import show_tags
+from tags.views import show_tags, show_tag
 
 urlpatterns = [
     path("", show_tags, name="tags_list"),
+    path("<int:primary_key>", show_tag, name="tag_detail"),
 ]
--- tags/views.py
+++ tags/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
 try:
     from tags.models import Tag
@@ -7,8 +7,19 @@ except Exception:
 
 
 # Create your views here.
-def show_tags(request):
+
+
+def show_tags(
+    request,
+):
     context = {
         "tags": Tag.objects.all() if Tag else None,
     }
     return render(request, "tags/list.html", context)
+
+
+def show_tag(request, primary_key):
+    tag = Tag.objects.get(pk=primary_key)
+    print(tag)
+
+    return render(request, "tags/detail.html")
