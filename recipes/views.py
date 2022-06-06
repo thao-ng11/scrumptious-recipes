ee5a4e7413e9f04ae4dd1f0d4e6363df6319d5e5
Thao <thao.ng11@gmail.com>
2022-06-06 15:35:27 -0400

    Added the update view for recipe

--- recipes/views.py
+++ recipes/views.py
@@ -1,7 +1,8 @@
+from audioop import reverse
 from django.shortcuts import redirect, render
 from django.urls import reverse_lazy
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView
 from django.views.generic.list import ListView
 
 from recipes.forms import RatingForm
@@ -35,23 +36,28 @@ class RecipeCreateView(CreateView):
 #     }
 #     return render(request, "recipes/new.html", context)
 
+class RecipeUpdateView(UpdateView):
+    model = Recipe
+    template_name = "recipes/edit.html"
+    fields = ["name", "author", "description", "image"]
+    success_url = reverse_lazy("recipes_list")
 
-def change_recipe(request, pk):
-    if Recipe and RecipeForm:
-        instance = Recipe.objects.get(pk=pk)
-        if request.method == "POST":
-            form = RecipeForm(request.POST, instance=instance)
-            if form.is_valid():
-                form.save()
-                return redirect("recipe_detail", pk=pk)
-        else:
-            form = RecipeForm(instance=instance)
-    else:
-        form = None
-    context = {
-        "form": form,
-    }
-    return render(request, "recipes/edit.html", context)
+# def change_recipe(request, pk):
+#     if Recipe and RecipeForm:
+#         instance = Recipe.objects.get(pk=pk)
+#         if request.method == "POST":
+#             form = RecipeForm(request.POST, instance=instance)
+#             if form.is_valid():
+#                 form.save()
+#                 return redirect("recipe_detail", pk=pk)
+#         else:
+#             form = RecipeForm(instance=instance)
+#     else:
+#         form = None
+#     context = {
+#         "form": form,
+#     }
+#     return render(request, "recipes/edit.html", context)
 
 
 class RecipeListView(ListView):
